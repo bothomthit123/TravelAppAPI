ac9e4b272b0e6fffeba7b582c59d6fbe2e2ee00b
bothomthit123 <120086697+bothomthit123@users.noreply.github.com>
2025-05-19 23:07:34 +0700

    Add files via upload

---
+++ Models/Recommend.cs
@@ -0,0 +1,23 @@
+﻿namespace TravelApp.Models
+{
+    public class Recommend
+    {
+    }
+    public class RecommendationRequest
+    {
+        public int AccountId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+    public class RecommendedPlaceDto
+    {
+        public int PlaceId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Category { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Score { get; set; } // Điểm gợi ý
+    }
+
+}
